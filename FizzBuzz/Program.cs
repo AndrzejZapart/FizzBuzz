9e9dfe86ed22911802ab671a15fcf12ff988cd7d
AndrzejZapart <andyas2022@gmail.com>
2022-10-04 21:09:25 +0200

    add a question about continue or not.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -12,12 +12,27 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             Console.WriteLine("Witamy w grze FizzBuzz. Wprowadź jakąkolwiek liczbę z klawiatury.\nPowiem Tobie czy jest podzielna.\njeśli bez reszty przez 3 to odpowiem Tobie hasłem Fizz.\njeśli bez reszty przez 5 to odpowiedm Tobie hasłem Buzz.\njeśli bez reszty przez 3 i 5 to odpowiem Tobie hasłem FizzBuzz.\na jeśli bez reszty ani przez 3 ani przez 5 to otrzymasz swoją liczbę z powrotem :). ");
+            var startagain = "t";
 
-            Console.WriteLine("Podaj swoją liczbę:");
-            var numberofgame = CheckNum();
-            var resultgame = new FizzBuzz();
-            var result = resultgame.CheckFizzBuzz(numberofgame);
-            Console.WriteLine($"Oto wynik naszej gry: {result}");
+            while (startagain == "t")
+            {
+                Console.WriteLine("Podaj swoją liczbę:");
+                var numberofgame = CheckNum();
+                var resultgame = new FizzBuzz();
+                var result = resultgame.CheckFizzBuzz(numberofgame);
+                Console.WriteLine($"Oto wynik naszej gry: {result}");
+                Console.WriteLine("Chcesz spróbować jeszcze raz ?? Wciśnij Tak wciśnij t, jeśli Nie wciśnij n.");
+
+                if (Console.ReadLine() == "t")
+                {
+                    continue;
+                }
+                return;
+            }
+            
+
+            
+
         }
 
         private static int CheckNum()
