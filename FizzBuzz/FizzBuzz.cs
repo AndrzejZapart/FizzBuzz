bf51600cc2164d0e4fe87e5a89a3e9b6ff9d0f47
Junior-Praca <Junior@DESKTOP-S3GS3KT>
2022-10-05 17:24:43 +0200

    first: correct formating code second optimize code to remove  variable name answer and give all to return mode.

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -6,34 +6,32 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
-        class FizzBuzz
-        {
-        public string answer;
+    class FizzBuzz
+    {
         public string CheckFizzBuzz(int numtocheck)
         {
-            if (numtocheck % 3 == 0  & numtocheck % 5 == 0)
+            if (numtocheck % 3 == 0 & numtocheck % 5 == 0)
             {
-                var answer = "FizzBuzz";
-                return answer;
+                return "FizzBuzz";
+
             }
             if (numtocheck % 3 == 0 & numtocheck % 5 != 0)
-                {
-                var answer = "Fizz";
-                return answer;
-                }
+            {
+                return "Fizz";
+
+            }
             if (numtocheck % 5 == 0 & numtocheck % 3 != 0)
             {
-                var answer = "Buzz";
-                return answer;
+                return "Buzz";
             }
-            else 
+            else
             {
-                var answer = numtocheck.ToString();
-                return answer;
+                return numtocheck.ToString();
+
             }
 
         }
-        }
-           
-   
+    }
+
+
 }
